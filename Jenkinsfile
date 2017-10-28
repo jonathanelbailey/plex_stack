6b86ef5ba6432c183795c2ad2cdce8a55264a7da
Jonathan Bailey <bailey.jonathan@hotmail.com>
2017-10-28 18:06:38 -0500

    testing metadata iso creation

--- Jenkinsfile
+++ Jenkinsfile
@@ -14,7 +14,9 @@ pipeline {
         os = 'ubuntu-17.10'
       }
       steps {
-        powershell '& $env:WORKSPACE\\create-vminstance.ps1 -vm_name test -vm_size 1x2 -os ubuntu-17.10 -verbose 4>&1'
+        powershell '& $env:WORKSPACE\\create-vminstance.ps1 -vm_name $env:vm_name -vm_size $env:vm_size -os $env:os -verbose 4>&1'
+        powershell '& $env:WORKSPACE\\create-cloudinit_disc.ps1 4>&1'
+        // powershell 'start-vm $emv:vm_name'
       }
     }
   }
---
+++ create-cloudinit_disc.ps1
@@ -0,0 +1,31 @@
+$ErrorActionPreference = "stop"
+
+$GuestAdminPassword = “P@ssw0rd”
+$path = 'c:\temp'
+$work = Join-Path $path -ChildPath 'work'
+$oscd_path = $path + 'oscdimg.exe'
+$unc_path = '\\nas1.magiccityit.com\os_iso\oscdimg.exe'
+$metadata_iso = Join-Path $path -ChildPath 'metadata.iso'
+if (!(Test-Path $oscd_path)){
+    cp $unc_path $oscd_path -Verbose
+}
+if (Test-Path $metadata_iso){
+    Remove-Item $metadata_iso -Force
+}
+$metadata = @”
+instance-id: $((get-vm -Name $env:vm_name).VMId)
+local-hostname: $($env:vm_name)
+“@
+ 
+$userdata = @”
+#cloud-config
+password: $($GuestAdminPassword)
+runcmd:
+ – [ useradd, -m, -p, “”, magicadmin ]
+ – [ chage, -d, 0, magicadmin ]
+“@
+md $work -Verbose
+sc “$($work)\meta-data” ([byte[]][char[]] “$metadata“) -Encoding Byte -Verbose
+sc “$($work)\user-data” ([byte[]][char[]] “$userdata“) -Encoding Byte -Verbose
+Start-Process oscdimg.exe -ArgumentList $work,$metadata_iso,"-j2 –lcidata" -Wait -Verbose
+Set-VMDvdDrive -VMName $env:vm_name -Path $metadata_iso -Verbose
