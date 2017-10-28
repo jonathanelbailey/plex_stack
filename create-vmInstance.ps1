2f541e333b56c840154747ee6a92e1b51a0c92bd
Jonathan Bailey <bailey.jonathan@hotmail.com>
2017-10-28 03:15:37 -0500

    added verbose

--- create-vmInstance.ps1
+++ create-vmInstance.ps1
@@ -21,26 +21,29 @@ switch ($os){
 $image_archive = $image + '.zip'
 $path = 'c:\temp'
 $unc_path = '\\nas1.magiccityit.com\os_iso'
-$unc_archive = Join-Path $unc_path -ChildPath $image_archive
-$archive_path = Join-Path -Path $path -ChildPath $image_archive
-$image_path = Join-Path -Path $path -ChildPath $image
-$gen2_image_path = $image_path + 'x'
+Write-Verbose $image_archive
+write-verbose $path
+write-verbose $unc_path
+$unc_archive = Join-Path $unc_path -ChildPath $image_archive -Verbose
+$archive_path = Join-Path -Path $path -ChildPath $image_archive -Verbose
+$image_path = Join-Path -Path $path -ChildPath $image -Verbose
+$gen2_image_path = $image_path + 'x' -Verbose
 
 if (!(Test-Path $archive_path)){
-    cp $unc_archive $archive_path
+    cp $unc_archive $archive_path -Verbose
 }
 if (!(Test-Path $image_path)){
-    Expand-Archive -Path $image_path -DestinationPath $path
+    Expand-Archive -Path $image_path -DestinationPath $path -Verbose
 }
 if (!(test-path $gen2_image_path)){
     Convert-VHD -Path $image_path -DestinationPath $gen2_image_path `
-        -VHDType Dynamic
+        -VHDType Dynamic -Verbose
 }
 # if(!(Get-VM -Name $vm_name -ErrorAction SilentlyContinue) -and $test ){
 #     Get-VM -Name $vm_name | Remove-VM
 # }
 
 new-vm -Name $vm_name -MemoryStartupBytes $memory -SwitchName "hyper-v" `
-    -VHDPath $gen2_image_path -Generation 2
-set-vm -Name $vm_name -ProcessorCount $cpu -StaticMemory
-Set-VMFirmware -VMName $vm_name -EnableSecureBoot Off
+    -VHDPath $gen2_image_path -Generation 2 -Verbose
+set-vm -Name $vm_name -ProcessorCount $cpu -StaticMemory -Verbose
+Set-VMFirmware -VMName $vm_name -EnableSecureBoot Off -Verbose
