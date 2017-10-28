6e2a51c133546eeb50ec6c4e6e9a9d4775ffa99c
Jonathan Bailey <bailey.jonathan@hotmail.com>
2017-10-28 18:26:00 -0500

    found issue with characters

--- create-cloudinit_disc.ps1
+++ create-cloudinit_disc.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-$GuestAdminPassword = “P@ssw0rd”
+$GuestAdminPassword = "P@ssw0rd"
 $path = 'c:\temp'
 $work = Join-Path $path -ChildPath 'work'
 $oscd_path = $path + 'oscdimg.exe'
@@ -12,20 +12,20 @@ if (!(Test-Path $oscd_path)){
 if (Test-Path $metadata_iso){
     Remove-Item $metadata_iso -Force
 }
-$metadata = \@”
+$metadata = @"
 instance-id: $((get-vm -Name $env:vm_name).VMId)
 local-hostname: $($env:vm_name)
-“\@
+"@
  
-$userdata = \@”
+$userdata = @"
 #cloud-config
 password: $($GuestAdminPassword)
 runcmd:
- – [ useradd, -m, -p, “”, magicadmin ]
+ – [ useradd, -m, -p, "", magicadmin ]
  – [ chage, -d, 0, magicadmin ]
-“\@
+"@
 md $work -Verbose
-sc “$($work)\meta-data” ([byte[]][char[]] “$metadata“) -Encoding Byte -Verbose
-sc “$($work)\user-data” ([byte[]][char[]] “$userdata“) -Encoding Byte -Verbose
+sc "$($work)\meta-data" ([byte[]][char[]] "$metadata") -Encoding Byte -Verbose
+sc "$($work)\user-data" ([byte[]][char[]] "$userdata") -Encoding Byte -Verbose
 Start-Process oscdimg.exe -ArgumentList $work,$metadata_iso,"-j2 –lcidata" -Wait -Verbose
 Set-VMDvdDrive -VMName $env:vm_name -Path $metadata_iso -Verbose
